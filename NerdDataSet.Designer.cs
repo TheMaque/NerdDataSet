efda182be194f8ff3599b7e2e6ad2b64a199753f
Reginald Telemaque <TelemaqueRS@cougars.sf.edu>
2020-04-22 14:15:44 -0400

    Changed colors again.

--- NerdDataSet.Designer.cs
+++ NerdDataSet.Designer.cs
@@ -78,7 +78,6 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.Desktop;
 			this.ClientSize = new System.Drawing.Size(852, 569);
 			this.Controls.Add(this.dgvInformation);
 			this.Controls.Add(this.pnlMain);
